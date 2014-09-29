11fdb1ed973147533686662b39aa3471f2e74116
henrical <bloodshotdawn55@gmail.com>
2014-09-29 18:36:32 -0300

    added random number generator

--- Aula 1/escritor.c
+++ Aula 1/escritor.c
@@ -6,28 +6,42 @@
 #define STRINGS_NUM 10
 #define FILES_NUM 5
 
-#define STRING_NAME_SIZE 10
-#define FILE_NAME_SIZE 13
+#define STRING_SIZE 16
 
 
-int main(int argc, char *argv[])
+//gera um inteiro aleatorio entre min e max
+int random_number(int min, int max)
 {
-	int i;
-	
-	char charSequencesArray[STRINGS_NUM][STRING_NAME_SIZE] =
-	 {"aaaaaaaaa\n", "bbbbbbbbb\n", "ccccccccc\n", "ddddddddd\n", "eeeeeeeee\n", "fffffffff\n", "ggggggggg\n", "hhhhhhhhh\n", "iiiiiiiii\n", "jjjjjjjjj\n"};
+	int result;
 
-	char fileNameArray[FILES_NUM][FILE_NAME_SIZE] = 
-	{"SO2014-0.txt", "SO2014-1.txt", "SO2014-2.txt", "SO2014-3.txt", "SO2014-4.txt"};
+	max = max + 1; 
+	
+	srand(time(NULL));
+	
+	result = (rand()%(max-min)) + min;
+
+	return result;
+}
+
+
+
+int main()
+{
+	int i = 0;
+	
+	char charSequencesArray[STRINGS_NUM][STRING_SIZE] = {"aaaaaaaaa\n", "bbbbbbbbb\n", "ccccccccc\n", "ddddddddd\n", "eeeeeeeee\n", "fffffffff\n", "ggggggggg\n", "hhhhhhhhh\n", "iiiiiiiii\n", "jjjjjjjjj\n"};
+
+	char fileNameArray[STRINGS_NUM][STRING_SIZE] =  {"SO2014-0.txt", "SO2014-1.txt", "SO2014-2.txt", "SO2014-3.txt", "SO2014-4.txt"};
 	 
 	FILE *pFile;
 
-	//escolhe o ficheiro onde vai escrever e a string que vais escrever
-	int fileNum; //random file name
-	int arrayNum; //random char sequence
-
-	fileNum = 3;
-	arrayNum = 2;
+	//escolhe aleatoriamente o ficheiro onde vai escrever e a string que vais escrever
+	int arrayNum; 
+	int fileNum;
+	
+	arrayNum = random_number(0,9); //random char sequence - escolhe uma das posiçoes do array aleatoriamente
+	fileNum = random_number(0,4); //random file name - escolhe uma das posiçoes do array aleatoriamente
+	
 
 	pFile = fopen( fileNameArray[fileNum], "w");
 	
